a2ad1d2cfe34897bfbb5903ac8c9c5e77e21fd75
SumitVermakgp <sumitverma9628@gmail.com>
2021-10-11 12:11:39 +0900

    set up basic end points

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const knex = require("./knex")
+
+
+const app = express();
+const port = 9999 || process.env.PORT;
+
+app.use(express.json());
+
+
+app.get("/api/v1/birthdays", async (_, res) => {
+    res.status(200).send("All Birthdays will be listed here!")
+});
+
+
+
+app.listen(9999, () => {
+    console.log(`Yaay, Server running at https://localhost:${port}!`);
+});
+
+
